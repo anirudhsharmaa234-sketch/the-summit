7ced9976285fbca5aa6acb9715de6836f0bdc4b5
anirudhsharmaa234-sketch <anirudhsharmaa234@gmail.com>
2026-08-29 22:56:24 +0530

    checkpoint: add audio control

---
+++ src/components/ExpeditionAudio.tsx
@@ -0,0 +1,2 @@
+import React from 'react';
+export const ExpeditionAudio:React.FC=()=> <div className="fixed bottom-5 left-5 z-50 border border-white/10 bg-black/50 px-3 py-2 text-[9px] tracking-[.2em] text-stone-500 uppercase">EXPEDITION / AUDIO READY</div>;
